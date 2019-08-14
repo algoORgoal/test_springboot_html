2631d80fbda348d35a2606c0eacaf1730946a2b2
algoORgoal <qufstar46@gmail.com>
2019-08-14 18:01:40 +0900

    Create a experiment file

---
+++ experiment.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class experiment {
+    public static void main(String[] args) {
+        Map<String, Object> resultObject = new HashMap<String, Object>();
+        resultObject.put("MEMBER_ID", "293029301212");
+        resultObject.put("CELLPHONE", "001-3923-6940");
+        resultObject.put("ABC", 123);
+        System.out.println(resultObject);
+    }
+}
